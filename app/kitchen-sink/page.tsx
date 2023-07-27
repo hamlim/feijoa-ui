43f6efe321d25e65090077b5a282ece67b874998
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-07-27 08:01:09 -0700

    [GEN-137] Add Stack component (#5)

--- app/kitchen-sink/page.tsx
+++ app/kitchen-sink/page.tsx
@@ -1,21 +1,24 @@
 import { Container } from '@components/ui/container'
 import { Heading } from '@components/ui/heading'
 import { BaseLink, Link } from '@components/ui/link'
+import { Stack } from '@components/ui/stack'
 import { Text } from '@components/ui/text'
 
 export default function KitchenSink() {
   return (
-    <div>
+    <Stack gap={4}>
       <Container>
         <Text>Container and Text!</Text>
       </Container>
       <Container>
-        <Heading is="h1">H1</Heading>
-        <Heading is="h2">H2</Heading>
-        <Heading is="h3">H3</Heading>
-        <Heading is="h4">H4</Heading>
-        <Heading is="h5">H5</Heading>
-        <Heading is="h6">H6</Heading>
+        <Stack gap={4}>
+          <Heading is="h1">H1</Heading>
+          <Heading is="h2">H2</Heading>
+          <Heading is="h3">H3</Heading>
+          <Heading is="h4">H4</Heading>
+          <Heading is="h5">H5</Heading>
+          <Heading is="h6">H6</Heading>
+        </Stack>
       </Container>
       <Container>
         <Link href="/">Local Link</Link>
@@ -24,6 +27,6 @@ export default function KitchenSink() {
           External Link!
         </BaseLink>
       </Container>
-    </div>
+    </Stack>
   )
 }

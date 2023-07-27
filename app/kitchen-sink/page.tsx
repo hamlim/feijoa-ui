9a57ad5b1944b4ccd1a17ef74e47d80580f4df2e
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-07-26 21:22:03 -0700

    Add Link component

--- app/kitchen-sink/page.tsx
+++ app/kitchen-sink/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@components/ui/container'
 import { Heading } from '@components/ui/heading'
+import { BaseLink, Link } from '@components/ui/link'
 
 export default function KitchenSink() {
   return (
@@ -12,6 +13,13 @@ export default function KitchenSink() {
         <Heading is="h4">H4</Heading>
         <Heading is="h5">H5</Heading>
         <Heading is="h6">H6</Heading>
+      </Container>
+      <Container>
+        <Link href="/">Local Link</Link>
+        <br />
+        <BaseLink href="https://matthamlin.me" target="_blank">
+          External Link!
+        </BaseLink>
       </Container>
     </div>
   )

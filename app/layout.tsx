e0dc5358c02c47336fb4f05e4c50c6b395b44d8d
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-09-11 21:10:31 -0400

    Add copy code functionality to the CodeBlock - less than ideal but it works

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@components/ui/theme-provider'
 import { Nav } from './Nav'
+import { Toaster } from '@components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function Layout({ children }) {
             <article>{children}</article>
             <footer></footer>
           </main>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
--- components/ui/code-block.tsx
+++ components/ui/code-block.tsx
@@ -1,6 +1,8 @@
 import { Code } from 'bright'
 import type { BrightProps, Extension } from 'bright'
 import { collapse } from './extensions/collapse-extension'
+import { CopyCode } from './extensions/copy-code'
+import { cn } from '@lib/utils'
 
 Code.theme = {
   dark: 'github-dark-dimmed',
@@ -14,5 +16,15 @@ interface Props extends Partial<BrightProps> {
 let defaultExtensions: Array<Extension> = [collapse]
 
 export function CodeBlock(props: Props) {
-  return <Code extensions={defaultExtensions} {...props} />
+  return (
+    <div className="relative overflow-scroll">
+      <Code
+        extensions={defaultExtensions}
+        lineNumbers
+        {...props}
+        className={cn('p-4 ', props.className)}
+      />
+      <CopyCode code={props.children} />
+    </div>
+  )
 }

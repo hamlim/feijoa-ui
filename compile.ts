4d5a63d9e555377fb584ee6da499e619f5e5dc04
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-10-28 19:25:32 -0400

    Add recpipe.json files for each recipe

--- compile.ts
+++ compile.ts
@@ -1,48 +1,7 @@
-import swc, { ImportDeclaration } from "@swc/core";
 import { execSync } from "child_process";
 import { readdir, readFile, writeFile } from "fs/promises";
 import path from "path";
 import type { RecipesMetadata } from "./packages/cli/types";
-
-async function parseImports(content): Promise<RecipesMetadata["recipes"][number]["dependencies"]> {
-  let ast = (await swc.parse(content, {
-    syntax: "typescript",
-    tsx: true,
-    comments: false,
-    script: true,
-    // Defaults to es3
-    target: "es3",
-  })).body;
-
-  let importDecls = ast.filter(node => {
-    return node.type === "ImportDeclaration";
-  }) as Array<ImportDeclaration>;
-
-  function justNames(decl: ImportDeclaration): string {
-    return decl.source.value;
-  }
-
-  function dedupe(arr: Array<string>): Array<string> {
-    return [...new Set(arr)];
-  }
-
-  return {
-    all: importDecls.map(justNames),
-    external: dedupe(
-      // Very naive
-      // Assumes the `foo/bar` means that the underlying dependency is `foo`
-      // but overlooks `@foo/bar/baz`!
-      importDecls.filter(decl => !decl.source.value.startsWith("./")).map(decl => {
-        let name = justNames(decl);
-        if (!name.startsWith("@") && name.includes("/")) {
-          return name.split("/")[0];
-        }
-        return name;
-      }),
-    ),
-    internal: dedupe(importDecls.filter(decl => decl.source.value.startsWith("./")).map(justNames)),
-  };
-}
 
 let recipesDir = "./packages/feijoa-ui/src/";
 
